488b283f5ba3e5c3bd037be6615998b8f939c75d
Lopes Maxime <lopes.max@ecucorp.eu>
2017-11-17 15:18:05 +0100

    Ajout du Chrono

--- src/pages/chrono/chrono.ts
+++ src/pages/chrono/chrono.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component }     from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Timer }         from '../../app/timer';
+import { State }         from '../../app/state';
+import { TwoDigit }      from '../../app/twodigits';
 
 @Component({
   selector: 'page-chrono',
   templateUrl: 'chrono.html'
 })
 export class Chrono {
+  private myTimer: Timer  = new Timer();
+  private myState: State  = new State();
+  private btnPlay: string = 'Demarrer';
 
   constructor(public navCtrl: NavController) {
 
+  }
+  start(){
+    this.myTimer.start();
+    this.myState.setPlay();
+  }
+  stop(){
+    this.myTimer.stop();
+    this.myState.setStop();
+  }
+  backward(){
+    this.myTimer.reset();
+    this.myState.setBackward();
   }
 
 }
